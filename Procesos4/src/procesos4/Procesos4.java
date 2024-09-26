49db5ca52a0d054632cda66cb21f459e94121ebb
manuel.soto <manuel.soto@2DAM-PROFESOR.mshome.net>
2024-09-26 16:14:48 +0200

    Primera sincronización de archivos

---
+++ Procesos4/src/procesos4/Procesos4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package procesos4;
+
+/**
+ *
+ * @author manuel.soto
+ */
+public class Procesos4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Proyecto conectado a GitHub");
+    }
+    
+}
